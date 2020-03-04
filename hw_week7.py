552e7b0695500d5da11e3b698cdcaa6f821a0e0e
bryantwu0419 <61681039+bryantwu0419@users.noreply.github.com>
2020-03-04 16:39:51 +0800

    Update hw_week7.py

--- hw_week7.py
+++ hw_week7.py
@@ -1,8 +1,26 @@
 import random
 
 def hw_week7(secret_number):
-    # Your code here
-    pass
+    import random
+    guessesTaken = 0
+    number = random.randint(1, 20)
+    print('Take a guess.') 
+    while guessesTaken < 6:
+        guess = input()
+        guess = int(guess)
+        guessesTaken = guessesTaken + 1
+        if guess < number:
+            print('Your guess is too low.') 
+        if guess > number:
+            print('Your guess is too high.')
+        if guess == number:
+            break
+    if guess == number:
+        guessesTaken = str(guessesTaken)
+        print('Good job! You guessed my number in ' + guessesTaken + ' guesses!')
+    if guess != number:
+       number = str(number)
+       print('Nope. The number I was thinking of was ' + number)
 
 
 if __name__ == '__main__':
